d09a9739198efdf43acda7ce4f7011cc861d1a41
carla cotrim <andrade.carla12@gmail.com>
2021-08-05 22:26:54 -0300

    changing from h2 database to mongodb

--- src/main/java/com/fatec/biblioteca/repositories/BookRepository.java
+++ src/main/java/com/fatec/biblioteca/repositories/BookRepository.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import com.fatec.biblioteca.models.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends MongoRepository<Book, Long> {
 
     Optional<Book> findByIsbn(String isbn);
     
--- src/main/java/com/fatec/biblioteca/repositories/UsuarioRepository.java
+++ src/main/java/com/fatec/biblioteca/repositories/UsuarioRepository.java
@@ -2,8 +2,9 @@ package com.fatec.biblioteca.repositories;
 
 import com.fatec.biblioteca.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UsuarioRepository extends JpaRepository<User, Long> {
+public interface UsuarioRepository extends MongoRepository<User, Long> {
 
     User findByUsername(String username);
 }
